cd62f3a7d26ba8f929b0c8f9766cf9b6d0ae99b7
Yash Gurav <87675745+itsyashprakash@users.noreply.github.com>
2023-01-22 14:52:18 +0530

    dice game project

---
+++ solvedByMe.js
@@ -0,0 +1,64 @@
+let randomNumber1 = Math.floor((Math.random()*6)+1);
+
+switch (randomNumber1) {
+    case 1:
+        document.querySelector('.img1').setAttribute('src', '/images/dice1.png');
+        break;
+    case 2:
+        document.querySelector('.img1').setAttribute('src', '/images/dice2.png');
+        break;
+    case 3:
+        document.querySelector('.img1').setAttribute('src', '/images/dice3.png');
+        break;
+    case 4:
+        document.querySelector('.img1').setAttribute('src', '/images/dice4.png');
+        break;
+    case 5:
+        document.querySelector('.img1').setAttribute('src', '/images/dice5.png');
+        break;
+    case 6:
+        document.querySelector('.img1').setAttribute('src', '/images/dice6.png');
+        break;
+
+    default:
+        console.log('refresh agian');
+        break;
+}
+
+
+
+let randomNumber2 = Math.floor((Math.random()*6)+1);
+
+switch (randomNumber2) {
+    case 1:
+        document.querySelector('.img2').setAttribute('src', '/images/dice1.png');
+        break;
+    case 2:
+        document.querySelector('.img2').setAttribute('src', '/images/dice2.png');
+        break;
+    case 3:
+        document.querySelector('.img2').setAttribute('src', '/images/dice3.png');
+        break;
+    case 4:
+        document.querySelector('.img2').setAttribute('src', '/images/dice4.png');
+        break;
+    case 5:
+        document.querySelector('.img2').setAttribute('src', '/images/dice5.png');
+        break;
+    case 6:
+        document.querySelector('.img2').setAttribute('src', '/images/dice6.png');
+        break;
+
+    default:
+        console.log('refresh agian');
+        break;
+}
+
+// logic to declare winner
+if(randomNumber1<randomNumber2){
+    document.querySelector('h1').textContent='Player 2 Wins';
+}else if(randomNumber1===randomNumber2){
+    document.querySelector('h1').textContent='Match Draw';
+}else{
+    document.querySelector('h1').textContent='Player 1 Wins';
+}
